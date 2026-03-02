6a09c514efc47893756141cf4a2583fb07a7dbec
Shamim Reja <shamimad23@gmail.com>
2026-03-02 15:24:35 +0530

    day-1   express basic setup

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.use(express.json()); // enable parsing of JSON request bodies
+
+// app.get("/", (req, res) => {
+//     res.send('Hello World on  !!');
+// })
+
+// // app.post("/users", (req, res) => {
+// //     res.json('user created !!')
+// // })
+
+// app.post("/users", (req, res) => {
+//     console.log(req.body);
+//     res.json({ received: req.body })
+// })
+
+app.use((req, res, next) => {
+    console.log('Middleware 1');
+    // next();
+})
+
+app.get("/", (req, res) => {
+    res.send('Home');
+})
+
+app.use((req, res, next) => {
+    console.log('Middleware 2');
+    next();
+})
+app.listen(port, () => {
+    console.log(`Example app listening on ${port}`);
+    
+})
